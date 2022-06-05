f6ae72f248ea848ef839a484e62b6a848bb7f39b
robert-gherlan <gherlanilierobert@gmail.com>
2022-06-05 12:34:22 +0300

    Updated the benchmark to cover more types of predicates.

--- src/main/java/com/robertg/benchmark/StreamsFilterBenchmark.java
+++ src/main/java/com/robertg/benchmark/StreamsFilterBenchmark.java
@@ -4,19 +4,32 @@ import com.github.javafaker.Faker;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class StreamsFilterBenchmark {
 
+    private static final int QUANTITY = 40;
+
+    private static final int PRICE = 2_000;
+
     private static final List<Product> PRODUCTS = getProducts();
+
+    private static final Predicate<Product> QUANTITY_PREDICATE = product -> product.quantity() > QUANTITY;
+
+    private static final Predicate<Product> PRICE_PREDICATE = product -> product.price() > PRICE;
+
+    private static final Predicate<Product> PRODUCTS_FILTER_QUANTITY_FIRST = QUANTITY_PREDICATE.and(PRICE_PREDICATE);
+
+    private static final Predicate<Product> PRODUCTS_FILTER_PRICE_FIRST = PRICE_PREDICATE.and(QUANTITY_PREDICATE);
 
     @Benchmark
     @Fork(value = 1, warmups = 1)
     @BenchmarkMode(Mode.Throughput)
     @Warmup(iterations = 1)
     @Measurement(iterations = 5)
-    public void benchmark_stream_multiple_filters() {
-        PRODUCTS.stream().filter(product -> product.quantity() > 40).filter(product -> product.price() > 2_000).count();
+    public void benchmark_stream_multiple_filters_quantity_first() {
+        PRODUCTS.stream().filter(product -> product.quantity() > QUANTITY).filter(product -> product.price() > PRICE).count();
     }
 
     @Benchmark
@@ -24,8 +37,44 @@ public class StreamsFilterBenchmark {
     @BenchmarkMode(Mode.Throughput)
     @Warmup(iterations = 1)
     @Measurement(iterations = 5)
-    public void benchmark_stream_single_filter() {
-        PRODUCTS.stream().filter(product -> product.quantity() > 40 && product.price() > 2_000).count();
+    public void benchmark_stream_multiple_filters_price_first() {
+        PRODUCTS.stream().filter(product -> product.price() > PRICE).filter(product -> product.quantity() > QUANTITY).count();
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @BenchmarkMode(Mode.Throughput)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 5)
+    public void benchmark_stream_single_filter_quantity_first() {
+        PRODUCTS.stream().filter(PRODUCTS_FILTER_QUANTITY_FIRST).count();
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @BenchmarkMode(Mode.Throughput)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 5)
+    public void benchmark_stream_single_filter_price_first() {
+        PRODUCTS.stream().filter(PRODUCTS_FILTER_PRICE_FIRST).count();
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @BenchmarkMode(Mode.Throughput)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 5)
+    public void benchmark_stream_single_predicate_quantity_first() {
+        PRODUCTS.stream().filter(product -> product.quantity() > QUANTITY && product.price() > PRICE).count();
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @BenchmarkMode(Mode.Throughput)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 5)
+    public void benchmark_stream_single_predicate_price_first() {
+        PRODUCTS.stream().filter(product -> product.price() > PRICE && product.quantity() > QUANTITY).count();
     }
 
     private static List<Product> getProducts() {
